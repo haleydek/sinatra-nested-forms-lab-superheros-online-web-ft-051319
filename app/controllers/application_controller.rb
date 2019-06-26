ee4c01542838f81ebbd73e71e98a37b5b1471bb7
Haley DeKeyser <hcd09@live.com>
2019-06-26 19:27:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
+    get '/' do
+      erb :super_hero
+    end
+    
+    post '/teams' do
+      Team.new(params[:team])
+      params[:heroes].each do |hero_hash|
+        Hero.new(hero_hash)
+      end
+      redirect '/teams'
+    end
+    
+    get '/teams' do
+      @team = Team.all 
+      @heroes = Hero.all
+      erb :team
+    end
 end
